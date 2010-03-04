6650e52b99b743e78b07f09b0e951c6ae7a60407
Morgan Wahl <morgan.wahl@noaa.gov>
2010-03-04 09:53:53 -0500

    Added a new affiliations formset to the Contact create and edit views/templates

--- cetacean_incidents/apps/contacts/views.py
+++ cetacean_incidents/apps/contacts/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
+from django.forms.formsets import formset_factory
 
 from models import Contact
-from forms import ContactForm
+from forms import ContactForm, OrganizationForm
 
 @login_required
 def _create_or_edit_contact(request, contact_id=None):
@@ -15,19 +16,32 @@ def _create_or_edit_contact(request, contact_id=None):
         # we're creating a new contact
         contact = None # the default for ContactForm(instance=)
         template = 'contacts/create_contact.html'
+    # the exisintg affiliations will be in the widget in ContactForm. This is
+    # only for adding new Organizations from within the same page
+    AffiliationsFormset = formset_factory(OrganizationForm, extra=2)
     
     if request.method == 'POST':
         form = ContactForm(request.POST, instance=contact)
-        if form.is_valid():
+        new_affiliations_formset = AffiliationsFormset(request.POST)
+        if form.is_valid() and new_affiliations_formset.is_valid():
             contact = form.save()
+            # add the affiliations from the new_affs_formset
+            for org_form in new_affiliations_formset.forms:
+                # don't save orgs with blank names.
+                if not 'name' in org_form.cleaned_data:
+                    continue
+                org = org_form.save()
+                contact.affiliations.add(org)
             return redirect('contact_detail', contact.id)
     else:
         form = ContactForm(instance=contact)
+        new_affiliations_formset = AffiliationsFormset()
     return render_to_response(
         template,
         {
             'contact': contact,
             'form': form,
+            'new_affiliations_formset': new_affiliations_formset,
         },
         context_instance= RequestContext(request),
     )
