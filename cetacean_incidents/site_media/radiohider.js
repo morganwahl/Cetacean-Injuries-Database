053ecefcdd0852fb305c2d2587e99fab621c7d86
Morgan Wahl <morgan.wahl@noaa.gov>
2010-04-29 15:41:30 -0400

    factored out radiohider.js

---
+++ cetacean_incidents/site_media/radiohider.js
@@ -0,0 +1,15 @@
+var RadioHider = {
+    'init': function(radio_name, val_to_id_map) {
+        var radio_sel = "[name=" + radio_name + "]";
+        $(radio_sel).change(function(event){
+            for (var val in val_to_id_map) {
+                var id = val_to_id_map[val];
+                if ($(this).val() == val)
+                    $('#' + id).show();
+                else
+                    $('#' + id).hide();
+            }
+        });
+    },
+}
+
