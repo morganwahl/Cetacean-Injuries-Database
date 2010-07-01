1fbe2569ebb6c7535be31b677a472b37def49c5c
Morgan Wahl <morgan.wahl@noaa.gov>
2010-07-01 11:16:41 -0400

    trivial changes

--- cetacean_incidents/apps/incidents/views.py
+++ cetacean_incidents/apps/incidents/views.py
@@ -1,8 +1,7 @@
 import operator
 
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.forms import Media
