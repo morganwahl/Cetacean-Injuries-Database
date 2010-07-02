b0fe11e32dc46ca4eda971bcf8d7ae8a5da6a164
Morgan Wahl <morgan.wahl@noaa.gov>
2010-07-02 11:48:11 -0400

    Fixed bug in checkboxhider.js

--- cetacean_incidents/site_media/checkboxhider.js
+++ cetacean_incidents/site_media/checkboxhider.js
@@ -2,12 +2,10 @@ var CheckboxHider = {
     'init': function(checkbox_name, element_id) {
         var checkbox_sel = "[name=" + checkbox_name + "]";
         $(checkbox_sel).change(function(event){
-            if ($(this).val() == 'on') {
+            if ($(this).attr("checked")) {
                 $('#' + element_id).show();
-            } else if ($(this).val() == '') {
-                $('#' + element_id).hide();
             } else {
-                alert("unknown val! :" + $(this).val());
+                $('#' + element_id).hide();
             }
         });
         $(checkbox_sel).change();
