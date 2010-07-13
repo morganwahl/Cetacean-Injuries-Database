636de5a9b9283de361c0f81d902e963fa60e153a
Morgan Wahl <morgan.wahl@noaa.gov>
2010-07-13 17:08:51 -0400

    ported vessel_details to display-tags

--- cetacean_incidents/apps/vessels/templatetags/vessel_extras.py
+++ cetacean_incidents/apps/vessels/templatetags/vessel_extras.py
@@ -1,6 +1,8 @@
 from django import template
+from django.template.loader import render_to_string
 
 from cetacean_incidents.apps.countries.utils.isoflag import iso_flag
+from cetacean_incidents.apps.generic_templates.templatetags.generic_field_display import display_row
 
 register = template.Library()
 
@@ -14,5 +16,22 @@ def flag_icon(country):
     return {
         'flag_url': iso_flag(country.iso),
         'country_name': unicode(country),
+    }
+
+@register.inclusion_tag('vessels/display_boatname_row.html')
+def display_boatname_row(vessel):
+    labels = []
+    values = []
+    if vessel.name:
+        labels.append('name')
+        values.append(vessel.name)
+    if vessel.flag:
+        labels.append('flag')
+        flag_html = render_to_string('flag_icon.html', flag_icon(vessel.flag))
+        values.append(flag_html)
+
+    return {
+        'labels': labels,
+        'values': values,
     }
 
