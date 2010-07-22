8499ac004aab8696e6002fff1b97bccad550e887
Morgan Wahl <morgan.wahl@noaa.gov>
2010-07-22 14:28:37 -0400

    tweaks to editing observation.wound fields

---
+++ cetacean_incidents/site_media/selecthider.js
@@ -0,0 +1,21 @@
+var SelectHider = {
+    'init': function(select_name, val_to_id_map) {
+        var select_sel = "[name=" + select_name + "]";
+        $(select_sel).change(function(event){
+            // multiple values can be used to show the same ID
+            shown_ids = {}
+            for (var val in val_to_id_map) {
+                var id = val_to_id_map[val];
+                if ($(this).val() == val) {
+                    $('#' + id).show();
+                    shown_ids[id] = true;
+                } else {
+                    if (! shown_ids[id])
+                        $('#' + id).hide();
+                }
+            }
+        });
+        $(select_sel).change();
+    }
+}
+
