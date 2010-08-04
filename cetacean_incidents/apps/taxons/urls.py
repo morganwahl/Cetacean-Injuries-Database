2cfc8931d21e57b45669b5f0a646f47608d9c4a8
Morgan Wahl <morgan.wahl@noaa.gov>
2010-08-04 10:05:00 -0400

    fixed bad merge

--- cetacean_incidents/apps/taxons/urls.py
+++ cetacean_incidents/apps/taxons/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls.defaults import *
 import views
-    url(r'^import$', views.taxon_import, name='taxon_import'),
-    url(r'^itis$', views.itis_search, name='itis'),
 
 urlpatterns = patterns('',
     url(r'^taxon_search$', views.taxon_search, name='taxon_search'),
+    url(r'^import$', views.taxon_import, name='taxon_import'),
+    url(r'^itis$', views.itis_search, name='itis'),
 )
 
