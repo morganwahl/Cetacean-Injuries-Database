79b8d2d3e36d741a2b5ad697929317b2b097b4ff
Morgan <morgan.wahl@noaa.gov>
2010-09-03 11:21:24 -0400

    IE Javascript hacks.

--- cetacean_incidents/site_media/radiohider.js
+++ cetacean_incidents/site_media/radiohider.js
@@ -1,6 +1,14 @@
 var RadioHider = {
     'init': function(radio_name, val_to_id_map) {
         var radio_sel = "[name=" + radio_name + "]";
+        // This is the hack for IE
+        // see http://stackoverflow.com/questions/208471
+        if ($.browser.msie) {
+          $(radio_sel).click(function() {
+            this.blur();
+            this.focus();
+          });
+        }
         $(radio_sel).change(function(event){
             for (var val in val_to_id_map) {
                 var id = val_to_id_map[val];
--- cetacean_incidents/site_media/selecthider.js
+++ cetacean_incidents/site_media/selecthider.js
@@ -1,6 +1,14 @@
 var SelectHider = {
     'init': function(select_name, val_to_id_map) {
         var select_sel = "[name=" + select_name + "]";
+        // This is the hack for IE
+        // see http://stackoverflow.com/questions/208471
+        if ($.browser.msie) {
+          $(select_sel).click(function() {
+            this.blur();
+            this.focus();
+          });
+        }
         $(select_sel).change(function(event){
             // multiple values can be used to show the same ID
             shown_ids = {}
