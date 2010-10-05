3a8e7f420ec85d21be12aa4d49bfb68ac9aa470d
Morgan Wahl <morgan.wahl@noaa.gov>
2010-10-05 18:26:08 -0400

    removed useless tests

--- cetacean_incidents/apps/incidents/tests.py
+++ cetacean_incidents/apps/incidents/tests.py
@@ -1,39 +1,11 @@
 from django.test import TestCase
 
-from forms import DateWidget
-
 from cetacean_incidents.apps.datetime.models import DateTime
 from models import Animal, Case, Observation
-
-class FormsTestCase(TestCase):
-    def setUp(self):
-        pass
-
-    def test_DateWidget(self):
-        dw = DateWidget()
-        # not sure how variable the exact HTML put out by the Django Media class
-        # is. The most robust way to testing would use BeautifulSoup to actually
-        # parse the HTML, but that means an extra dependency just for testing.
-        
-        # for now, assume one script per line of output.
-        js_string = unicode(dw.media['js'])
-        js_lines = len(js_string.split("\n"))
-        self.assertEqual(js_lines, 4)
 
 class CaseManagerTestCase(TestCase):
     
     fixtures = ['case-date-test']
-    
-    def test_cases_in_year(self):
-        for (year, case_ids) in {
-            1982: [],
-            1992: (6,),
-            2008: (5,),
-            2009: (2,),
-            2010: (1,3,4),
-        }.items():
-            cases = Case.objects.filter(id__in=case_ids)
-            self.assertEqual(set(Case.objects.cases_in_year(year)), set(cases))
     
     def test_same_timeframe(self):
         for (case_id, result_ids) in {
