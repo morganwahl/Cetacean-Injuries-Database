a07ff5c7912b1920460873ef87dbac4c1ffa3977
Morgan Wahl <morgan.wahl@noaa.gov>
2010-10-28 12:30:33 -0400

    removing obsolete TaxonWidget

--- cetacean_incidents/apps/taxons/forms.py
+++ cetacean_incidents/apps/taxons/forms.py
@@ -9,54 +9,6 @@ from cetacean_incidents.apps.merge_form.forms import MergeForm
 
 from models import Taxon
 
-class TaxonWidget(forms.widgets.Widget):
-    '''\
-    A widget that searches Taxons while you type and allows you to select one
-    of them.
-    '''
-    
-    input_type = 'hidden'
-    
-    def render(self, name, value, attrs=None):
-        """
-        Returns this Widget rendered as HTML, as a Unicode string.
-
-        The 'value' given is not guaranteed to be valid input, so subclass
-        implementations should program defensively.
-        """
-        
-        none_display_name = 'none chosen'
-        
-        if value in EMPTY_VALUES:
-            value = ''
-            taxon_value = None
-            initial_display_name = none_display_name
-        else:
-            try:
-                taxon_value = Taxon.objects.get(id=value)
-                value = unicode(taxon_value.id)
-                initial_display_name = unicode(taxon_value)
-            except Taxon.DoesNotExist:
-                value = ''
-                taxon_value = None
-                initial_display_name = none_display_name
-                print "No good value: %s" % repr(value)
-        
-        final_attrs = self.build_attrs(attrs, type=self.input_type, name=name, value=value)
-        
-        # assumes the the django.template.loaders.app_directories.load_template_source 
-        # is being used, which is the default.
-        return render_to_string('taxon_widget.html', {
-            'none_display_name': none_display_name,
-            'initial_display_name': initial_display_name,
-            'final_attrs': forms.util.flatatt(final_attrs),
-            'name': name,
-        })
-    
-    class Media:
-        css = {'all': ('taxon_widget.css',)}
-        js = (settings.JQUERY_FILE, 'taxon_widget.js')
-
 class TaxonAutocomplete(ModelAutocomplete):
     
     model = Taxon
@@ -88,7 +40,7 @@ class TaxonAutocomplete(ModelAutocomplete):
 
 class TaxonField(forms.Field):
     # based on ModelChoiceField, except you can't choose queryset, and there
-    # are no choices, since we're using an AJAX-y TaxonWidget
+    # are no choices, since we're using an AJAX-y TaxonAutocomplete
     
     # a Field's widget defaults to self.widget
     widget = TaxonAutocomplete
