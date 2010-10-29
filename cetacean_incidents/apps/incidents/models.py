ef87b8033560a25f758269357f8edf998f5a1d2f
Morgan Wahl <morgan.wahl@noaa.gov>
2010-10-29 17:03:00 -0400

    trival changes

--- cetacean_incidents/apps/incidents/models.py
+++ cetacean_incidents/apps/incidents/models.py
@@ -515,7 +515,7 @@ class Case(models.Model):
             return rep_dates[0]
         else:
             return None
-    
+
     date = first_observation_date
     
     def earliest_datetime(self):
@@ -538,9 +538,10 @@ class Case(models.Model):
     
     def associated_cases(self):
         return Case.objects.associated_cases(self)
-
+    
     def clean(self):
         if not self.nmfs_id is None and self.nmfs_id != '':
+            # TODO do they have to be unique or not?
             # check that an existing case doesn't already have this nmfs_id
             cases = Case.objects.filter(nmfs_id=self.nmfs_id)
             if self.id:
