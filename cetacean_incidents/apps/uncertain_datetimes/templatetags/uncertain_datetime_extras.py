ff206ffb06d42db62e427fe41e8859322f073ba1
Morgan Wahl <morgan.wahl@noaa.gov>
2010-11-02 11:50:08 -0400

    fixed uncertaindatetime_sort_key template tag

--- cetacean_incidents/apps/uncertain_datetimes/templatetags/uncertain_datetime_extras.py
+++ cetacean_incidents/apps/uncertain_datetimes/templatetags/uncertain_datetime_extras.py
@@ -2,29 +2,13 @@ from django import template
 
 register = template.Library()
 
-@register.inclusion_tag('datetime_sortkey.html')
-def datetime_sort_key(datetime):
+@register.simple_tag
+def uncertaindatetime_sort_key(uncertaindatetime):
     '''\
-    Include a key that can be sorted lexicographically (or numerically). If
-    datetime is None, the key will sort _after_ all other keys. (i.e. unknown
-    date are considered to be less recent.) Datetime can be our own DateTime
-    model instances, or a Python datetime object.
+    Include a key that can be sorted lexicographically (or numerically). a part
+    of the date is None, it will sort _after_ datetimes that are otherwise 
+    identical (i.e. unknown dates are considered to be less recent.)
     '''
     
-    d = {}
-    if datetime is None:
-        for k in ('year', 'month', 'day', 'hour', 'minute', 'second'):
-            d[k] = None
-    else:
-        # this works with both our DateTimes and the Python built-in datetime class
-        # since only ours can have None values for this attributes
-        d['year'] = datetime.year if not datetime.year is None else 0
-        d['month'] = datetime.month if not datetime.month is None else 0
-        d['day'] = datetime.day if not datetime.day is None else 0
-        # note that hours, minutes and seconds have to be offset by 1 since
-        # '0' is a defined value for them.
-        d['hour'] = datetime.hour + 1 if not datetime.hour is None else 0
-        d['minute'] = datetime.minute + 1 if not datetime.minute is None else 0
-        d['second'] = datetime.second + 1 if not datetime.second is None else 0
-    
-    return { 'd': d }
+    return uncertaindatetime.sortkey(unknown_is_later=True)
+
