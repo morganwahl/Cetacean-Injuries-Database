f8a0f35c925cf41734649c8da4d60d3aef788b12
Morgan Wahl <morgan.wahl@noaa.gov>
2010-11-02 11:47:48 -0400

    adding back the old DateTime app

---
+++ cetacean_incidents/apps/datetime/tests.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+
+from datetime import datetime, timedelta
+import pytz
+from models import UncertainDateTime
+
+class UncertainDateTimeTestCase(TestCase):
+    def setUp(self):
+        self.me = UncertainDateTime(year=1982,month=3,day=20)
+        self.leapfeb = UncertainDateTime(year=2004, month=2)
+        self.regfeb = UncertainDateTime(year=1900, month=2)
+        self.leapday = UncertainDateTime(year=2004, month=2, day=29)
+
+    def test_earliest(self):
+        self.assertEquals(self.me.earliest, datetime(1982, 3, 20, 0, 0, 0, 0, pytz.utc))
+        self.assertEquals(self.leapfeb.earliest, datetime(2004, 2, 1, 0, 0, 0, 0, pytz.utc))
+        self.assertEquals(self.regfeb.earliest, datetime(1900, 2, 1, 0, 0, 0, 0, pytz.utc))
+        self.assertEquals(self.leapday.earliest, datetime(2004, 2, 29, 0, 0, 0, 0, pytz.utc))
+
+    def test_latest(self):
+        self.assertEquals(self.me.latest, datetime(1982, 3, 21, 0, 0, 0, 0, pytz.utc))
+        self.assertEquals(self.leapfeb.latest, datetime(2004, 3, 1, 0, 0, 0, 0, pytz.utc))
+        self.assertEquals(self.regfeb.latest, datetime(1900, 3, 1, 0, 0, 0, 0, pytz.utc))
+        self.assertEquals(self.leapday.latest, datetime(2004, 3, 1, 0, 0, 0, 0, pytz.utc))
+    
+    def test_breadth(self):
+        self.assertEquals(self.me.breadth, timedelta(days=1))
+        self.assertEquals(self.leapfeb.breadth, timedelta(days=29))
+        self.assertEquals(self.regfeb.breadth, timedelta(days=28))
+        self.assertEquals(self.leapday.breadth, timedelta(days=1))
+
