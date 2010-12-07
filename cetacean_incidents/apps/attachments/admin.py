10d69719b2203025eac1436d5449da7f4963bf6c
Morgan Wahl <morgan.wahl@noaa.gov>
2010-12-07 12:01:37 -0500

    fixed attachment admin

--- cetacean_incidents/apps/attachments/admin.py
+++ cetacean_incidents/apps/attachments/admin.py
@@ -1,14 +1,21 @@
 from django.contrib import admin
 from reversion.admin import VersionAdmin
-from models import AttachmentType, Attachment
+from models import AttachmentType, Attachment, UploadedFile, RepositoryFile
 
 class AttachmentTypeAdmin(VersionAdmin):
     pass
 admin.site.register(AttachmentType, AttachmentTypeAdmin)
 
 class AttachmentAdmin(VersionAdmin):
-    list_display = ('__unicode__', 'storage_type', 'name', 'attachment_type')
+    list_display = ('__unicode__', 'attachment_type')
     list_display_links = ('__unicode__',)
-    list_filter = ('storage_type', 'attachment_type', 'repo')
+    list_filter = ('attachment_type',)
 admin.site.register(Attachment, AttachmentAdmin)
 
+class UploadedFileAdmin(AttachmentAdmin):
+    list_display = AttachmentAdmin.list_display + ('uploader', 'datetime_uploaded')
+    list_filter = AttachmentAdmin.list_filter + ('uploader', 'datetime_uploaded')
+admin.site.register(UploadedFile, UploadedFileAdmin)
+class RepositoryFileAdmin(AttachmentAdmin):
+    list_filter = AttachmentAdmin.list_filter + ('repo',)
+admin.site.register(RepositoryFile, RepositoryFileAdmin)
