b0b592692992d2699e205c43b7cf8b5202d4b5e3
Morgan Wahl <morgan.wahl@noaa.gov>
2010-12-07 12:11:33 -0500

    added ObservationAttachment to Admin

--- cetacean_incidents/apps/incidents/admin.py
+++ cetacean_incidents/apps/incidents/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from reversion.admin import VersionAdmin
-from models import Animal, Case, Observation, YearCaseNumber
+from models import Animal, YearCaseNumber, Case, Observation, ObservationAttachment
 
 class AnimalAdmin(VersionAdmin):
     pass
@@ -13,6 +13,10 @@ admin.site.register(Case, CaseAdmin)
 class ObservationAdmin(VersionAdmin):
     pass
 admin.site.register(Observation, ObservationAdmin)
+
+class ObservationAttachmentAdmin(VersionAdmin):
+    pass
+admin.site.register(ObservationAttachment, ObservationAttachmentAdmin)
 
 # YearCaseNumber doesn't really belong in the admin interface (it's
 # automatically maintained), but it's handy for fixing screw-ups. Also, the 
