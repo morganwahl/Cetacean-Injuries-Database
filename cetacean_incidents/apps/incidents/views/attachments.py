ad4138f5ea0345b7da2919a4293a3eb7a4749160
Morgan Wahl <morgan.wahl@noaa.gov>
2010-12-14 11:24:29 -0500

    re-write of document-adding views

--- cetacean_incidents/apps/incidents/views/attachments.py
+++ cetacean_incidents/apps/incidents/views/attachments.py
@@ -6,49 +6,89 @@ from django.template import RequestContext
 
 from cetacean_incidents.decorators import permission_required
 
+from cetacean_incidents.documents.forms import (
+    DocumentModelForm,
+    DocumentForm,
+    UploadedFileForm,
+    RepositoryFileForm,
+)
+
 from ..models import Case, CaseDocument, Observation, ObservationDocument
 
-def _get_documentforms(request):
+def _add_attachment(attachment_class, obj, template='incidents/add_attachment.html', obj_name = 'object'):
+    
     form_classes = {
         'model': DocumentModelForm,
         'document': DocumentForm,
         'uploaded_file': UploadedFileForm,
         'repository_file': RepositoryFileForm,
     }
-    
+
     form_kwargs = {}
     for name in form_classes.keys():
         form_kwargs[name] = {
             'prefix': name,
         }
     
-    if request.method == 'POST':
-        for name in form_classes.keys():
-            form_kwargs[name]['data'] = request.POST
-        form_kwargs['uploaded_file']['files'] = request.FILES
-    
     forms = {}
-    for name, cls in form_classes.items():
-        forms[name] = cls(**form_kwargs[name])
     
-    return forms
-
-def _save_documentforms(request, forms):
-    if forms['model'].is_valid():
-        docform = {
-            'Document': forms['document'],
-            'UploadedFile': forms['uploaded_file'],
-            'RepositoryFile': forms['repository_file'],
-        }[forms['model'].cleaned_data['storage_type']]
-        
-        if docform.is_valid():
-            doc = docform.save(commit=False)
-            if forms['model'].cleaned_data['storage_type'] == 'UploadedFile':
-                doc.uploader = request.user
-            doc.save()
-            docform.save_m2m()
+    if request.method == 'POST':
+        # we have to instantiate the 'model' form in order to know which other
+        # forms to bind
+        form_kwargs['model']['data'] = request.POST
+        forms['model'] = form_classes['model'](**form_kwargs['model'])
+        # we only want to bind the forms the user acutally filled in
+        if forms['model'].is_valid():
+            st = forms['model'].cleaned_data['storage_type']
+            if st == 'Document':
+                submitted_name = 'document'
+            if st == 'UploadedFile':
+                submitted_name = 'uploaded_file'
+                form_kwargs['uploaded_file']['files'] = request.FILES
+            if st == 'RepositoryFile':
+                submitted_name = 'repository_file'
+            form_kwargs[submitted_name]['data'] = request.POST
             
-            return doc
+            # instantiate all the forms in case there's an error in the one that
+            # was submitted
+            for name in ('document', 'uploaded_file', 'repository_file'):
+                forms[name] = form_classes[name](**form_kwargs[name])
+                
+            # now check the submitted one 
+            if forms[submitted_name].is_valid():
+                document = forms[submitted_name].save(commit=False)
+                if submitted_name == 'uploaded_file':
+                    document.uploader = request.user
+                document.save()
+                forms[submitted_name].save_m2m()
+                
+                attachment = attachment_class.objects.create(
+                    document= document,
+                    attached_to= obj,
+                )
+                return redirect(obj)
+                
+    else: # request.method != 'POST'
+        for name in forms_classes.keys():
+            forms[name] = form_classes[name](**form_kwargs[name])
+    
+    # the default template uses the radiohider script with forms['models']
+    # select widget. TODO currently no way to pass in a custom Media to go with 
+    # a custom template.
+    template_media = Media(
+        js= (settings.JQUERY_FILE, 'radiohider.js'),
+    )
+    media = reduce( lambda m, f: m + f.media, forms.values(), template_media)
+
+    return render_to_response(
+        template,
+        {
+            obj_name: obj,
+            'forms': forms,
+            'media': media,
+        },
+        context_instance= RequestContext(request),
+    )
 
 @login_required
 @permission_required('documents.add_document')
@@ -56,61 +96,13 @@ def add_casedocument(request, case_id):
     
     c = Case.objects.get(id=case_id)
     
-    forms = _get_documentforms(request)
+    return _add_attachment(CaseDocument, c)
     
-    if request.method == 'POST':
-        doc = _save_documentforms(request, forms)
-        if doc:
-            case_doc = CaseDocument.objects.create(
-                document= doc,
-                attached_to= c,
-            )
-            return redirect(c)
-    
-    template_media = Media(
-        js= (settings.JQUERY_FILE, 'radiohider.js'),
-    )
-    media = reduce( lambda m, f: m + f.media, forms.values(), template_media)
-    
-    return render_to_response(
-        'incidents/add_casedocument.html',
-        {
-            'case': c,
-            'forms': forms,
-            'media': media,
-        },
-        context_instance= RequestContext(request),
-    )
-
 @login_required
 @permission_required('documents.add_document')
 def add_observationdocument(request, observation_id):
     
     o = Observation.objects.get(id=observation_id)
     
-    forms = _get_documentforms(request)
-    
-    if request.method == 'POST':
-        doc = _save_documentforms(request, forms)
-        if doc:
-            obs_doc = ObservationDocument.objects.create(
-                document= doc,
-                attached_to= o,
-            )
-            return redirect(o)
-    
-    template_media = Media(
-        js= (settings.JQUERY_FILE, 'radiohider.js'),
-    )
-    media = reduce( lambda m, f: m + f.media, forms.values(), template_media)
-    
-    return render_to_response(
-        'incidents/add_observationdocument.html',
-        {
-            'observation': o,
-            'forms': forms,
-            'media': media,
-        },
-        context_instance= RequestContext(request),
-    )
+    return _add_attachment(ObservationDocument, o)
 
