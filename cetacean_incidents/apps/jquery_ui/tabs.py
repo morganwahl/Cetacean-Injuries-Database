f2a5d9262f98a1da3cc069a7e4e5b20ff745a4cd
Morgan Wahl <morgan.wahl@noaa.gov>
2011-02-08 18:37:07 -0500

    big redesign of Tab API

--- cetacean_incidents/apps/jquery_ui/tabs.py
+++ cetacean_incidents/apps/jquery_ui/tabs.py
@@ -2,37 +2,60 @@ from django.forms.widgets import MediaDefiningClass
 from django.conf import settings
 from django.template import Context
 from django.template.loader import render_to_string
+from django.template.loader import get_template
 
 class Tab(object):
     '''\
     A Tab has a html_name, a display name, a template and a context.
     '''
     
-    def __init__(self, html_id, template, context=Context(), html_display=None, error=False):
+    default_html_display = None
+
+    @property
+    def default_template(self):
+        raise NotImplementedError
+
+    required_context_keys = tuple()
+    
+    def __init__(self, html_id, context, template=None, html_display=None):
         '''\
         If display_name is none is defaults to the html_name with '_'s replaced
         by spaces and titlecased.
+        Error_func is a callable that is passed the same context as render_li.
+        If it returns False, the tab will be highlighted to indicate an error.
         '''
 
         self.html_id = html_id
+
+        if html_display is None:
+            html_display = self.default_html_display
         if html_display is None:
             html_display = html_id
             html_display = html_display.replace('_', ' ')
             html_display = html_display.title()
         self.html_display = html_display
-
+        
+        if template is None:
+            template = self.default_template
         self.template = template
+        
+        # check that all the required keys are there
+        for k in self.required_context_keys:
+            if not k in context:
+                raise ValueError("%s was initiated with a context without a required key: %s" % (self.__class__.__name__, k))
         self.context = context
-        self.error = error
         
     def render_js(self):
         return render_to_string("tab_js.js", {'tab': self})
 
-    def render_tab(self):
-        return render_to_string("tab_li.html", {'tab': self, 'error': self.error})
+    def li_error(self):
+        return False    
+    
+    def render_li(self):
+        return render_to_string("tab_li.html", {'tab': self, 'error': self.li_error()})
 
     def render_body(self):
-        return self.template.render(self.context)
+        return get_template(self.template).render(self.context)
 
 class Tabs(object):
     '''\
@@ -46,23 +69,35 @@ class Tabs(object):
         css = {'all': (settings.JQUERYUI_CSS_FILE,)}
         js = (settings.JQUERY_FILE, settings.JQUERYUI_JS_FILE)
     
-    def __init__(self, tabs=tuple()):
+    def __init__(self, tabs, html_id='tabs'):
         # TODO check that tabs is an iterable of Tab instances
         # TODO check that all the html_names differ
         
+        self.html_id = html_id
+
+        self.tab_data = []
         # add the prev and next IDs
         prev = None
-        for tab in tabs:
-            if prev:
-                prev.next_id = tab.html_id
-                tab.prev_id = prev.html_id
-            prev = tab
+        for i in range(len(tabs)):
+            data = {
+                'tab': tabs[i],
+            }
             
-        self.tabs = tabs
+            if prev:
+                prev['next_id'] = data['tab'].html_id
+                data['prev_id'] = prev['tab'].html_id
+
+            prev = data
+            self.tab_data.append(data)
     
     def render(self):
         '''\
         Returns the HTML for the tabs.
         '''
-        return render_to_string("tabs.html", {'tabs': self.tabs})
+        
+        for data in self.tab_data:
+            data['li'] = data['tab'].render_li()
+            data['body'] = data['tab'].render_body()
+        
+        return render_to_string("tabs.html", {'tabs': self})
 
