735260bb98fe1724256e2b676c3414422cc66bed
Morgan Wahl <morgan.wahl@noaa.gov>
2011-02-25 14:53:52 -0500

    trivial changes

--- cetacean_incidents/apps/strandings_import/views.py
+++ cetacean_incidents/apps/strandings_import/views.py
@@ -1,13 +1,10 @@
-import os
-from datetime import datetime
-import pytz
-from copy import copy
-
 from django.contrib.auth.decorators import login_required
+from django.db import transaction
+from django.shortcuts import redirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.utils.html import conditional_escape as esc
-from django.shortcuts import redirect
+
+from reversion import revision
 
 from cetacean_incidents.decorators import permission_required
 
@@ -16,14 +13,17 @@ from csv_import import parse_csv, process_results
     
 # TODO perms
 @login_required
+@transaction.commit_on_success
+@revision.create_on_success
 def import_csv(request):
     
     results = None
     if request.method == 'POST':
         form = ImportCSVForm(request.POST, request.FILES)
         if form.is_valid():
-            results = parse_csv(form.cleaned_data['csv_file'])
             
+            results = parse_csv(form.cleaned_data['csv_file'])
+    
             if not form.cleaned_data['test_run']:
                 process_results(results, form.cleaned_data['csv_file'].name, request.user)
                 return redirect('home')
