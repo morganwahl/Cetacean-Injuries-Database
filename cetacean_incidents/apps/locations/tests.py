11493d6e816409e5bfb49c0b2fd202e9c7059310
Morgan Wahl <morgan.wahl@noaa.gov>
2011-02-25 15:31:44 -0500

    fixed location tests

--- cetacean_incidents/apps/locations/tests.py
+++ cetacean_incidents/apps/locations/tests.py
@@ -1,13 +1,16 @@
 from django.test import TestCase
 
+from decimal import Decimal as D
+
 from utils import dms_to_dec, dec_to_dms
 
 class UtilsTestCase(TestCase):
     def testDmsToDec(self):
-        self.assertEquals(dms_to_dec((True, 70, 30, 0)), -70.5)
+        self.assertEquals(dms_to_dec((True, 70, 30, 0)), D('-70.5'))
         self.assertEquals(
-            dms_to_dec((False, 32, 19, 24.04)),
-            32 + (19 / 60.0) + (24.04 / (60 * 60))
+            # only test to millionth of a degree
+            dms_to_dec((False, 32, 19, 24.04)).quantize(D('0.000001')),
+            (D('32') + (D('19') / 60) + (D('24.04') / (60 * 60))).quantize(D('0.000001'))
         )
 
 from models import Location
@@ -26,11 +29,11 @@ class NiceLocationFormTestCase(TestCase):
             form.cleaned_data['coordinates_lat_input'],
             # TODO how do we deal with rounding errors? or should the test not
             # pass if they make a difference?
-            (False, float(32), float(19), float(24.04))
+            (False, D(32), D(19), D('24.04'))
         )
         self.assertEquals(
             form.cleaned_data['coordinates_lng_input'],
-            (True, float(71.5), 0.0, 0.0)
+            (True, D('71.5'), D(0), D(0))
         )
         form = NiceLocationForm({
             'coordinates_lat_input': '32 19 24.04',
@@ -42,11 +45,11 @@ class NiceLocationFormTestCase(TestCase):
             form.cleaned_data['coordinates_lat_input'],
             # TODO how do we deal with rounding errors? or should the test not
             # pass if they make a difference?
-            (False, float(32), float(19), float(24.04))
+            (False, D(32), D(19), D('24.04'))
         )
         self.assertEquals(
             form.cleaned_data['coordinates_lng_input'],
-            (True, float(71.5), 0.0, 0.0)
+            (True, D('71.5'), D(0), D(0))
         )
     
     def testSave(self):
