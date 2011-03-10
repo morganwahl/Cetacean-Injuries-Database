cec99616ea96f30f5c0ec4fba1621a3409b01e5c
Morgan Wahl <morgan.wahl@noaa.gov>
2011-03-10 09:53:03 -0500

    moving ObservationAction fields to ObservationIncident

--- cetacean_incidents/apps/incidents/views/observation.py
+++ cetacean_incidents/apps/incidents/views/observation.py
@@ -60,7 +60,6 @@ from tabs import (
     ObservationIncidentTab,
     ObservationNarrativeTab,
     ObservationObservingTab,
-    ObservationActionsTab,
 )
 
 @login_required
@@ -405,7 +404,6 @@ def _change_incident(
         ObservationObservingTab(html_id='observation-observing'),
         ObservationAnimalIDTab(html_id='observation-animal'),
         ObservationIncidentTab(html_id='observation-incident'),
-        ObservationActionsTab(html_id='observation-actions'),
     ] + additional_observation_tabs +[
         ObservationNarrativeTab(html_id='observation-narrative'),
     ]
--- cetacean_incidents/apps/incidents/views/tabs.py
+++ cetacean_incidents/apps/incidents/views/tabs.py
@@ -138,26 +138,6 @@ class ObservationIncidentTab(Tab):
                         'indication_shipstrike',
                         'wounded',
                         'wound_description',
-                    )
-                )
-            )
-        )
-
-class ObservationActionsTab(Tab):
-    
-    default_html_display = mark_safe(u"<em>Observation</em><br>Actions")
-    default_template = 'incidents/edit_observation_actions_tab.html'
-    required_context_keys = ('forms',)
-    
-    def li_error(self):
-        return reduce(
-            operator.or_, map(
-                bool,
-                [
-                    self.context['forms']['observation'].non_field_errors(),
-                ] + map(
-                    lambda f: self.context['forms']['observation'][f].errors, 
-                    (
                         'documentation',
                         'tagged',
                         'biopsy',
