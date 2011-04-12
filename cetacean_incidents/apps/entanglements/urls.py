ab3d627da8102f14b5c699f40cf8f5137fe00f3c
Morgan Wahl <morgan.wahl@noaa.gov>
2011-04-12 11:01:56 -0400

    removed redundant add_gear_owner view

--- cetacean_incidents/apps/entanglements/urls.py
+++ cetacean_incidents/apps/entanglements/urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
     (r'^(?P<destination_id>\d+)/merge$', views.entanglement_merge, {}, 'entanglement_merge'),
     (r'^(?P<destination_id>\d+)/merge/(?P<source_id>\d+)$', views.entanglement_merge, {}, 'entanglement_merge'),
 
-    (r'^(\d+)/add_gear_owner$', views.add_gear_owner, {}, 'add_gear_owner'),
+    (r'^(\d+)/add_gear_owner$', views.edit_gear_owner, {}, 'add_gear_owner'),
     (r'^(\d+)/edit_gear_owner$', views.edit_gear_owner, {}, 'edit_gear_owner'),
     
     # TODO make a permanent redirect to generic observation detail
--- cetacean_incidents/apps/entanglements/views.py
+++ cetacean_incidents/apps/entanglements/views.py
@@ -140,70 +140,6 @@ def edit_entanglement(request, entanglement_id):
 @login_required
 @permission_required('entanglements.change_entanglement')
 @permission_required('entanglements.view_gear_owner')
-@permission_required('entanglements.add_gear_owner')
-def add_gear_owner(request, entanglement_id):
-    # TODO merge in with edit_gear_owner
-    entanglement = Entanglement.objects.get(id=entanglement_id)
-
-    form_classes = {
-        'gear_owner': GearOwnerForm,
-        'location_set': NiceLocationForm,
-    }
-    forms = {}
-    for form_name, form_class in form_classes.items():
-        kwargs = {}
-        if request.method == 'POST':
-            kwargs['data'] = request.POST
-        forms[form_name] = form_class(prefix=form_name, **kwargs)
-            
-    if request.method == 'POST':
-        class _SomeValidationFailed(Exception):
-            pass
-        class _NothingToSave(Exception):
-            pass
-        def _check(form_name):
-            if not forms[form_name].is_valid():
-                raise _SomeValidationFailed(form_name, forms[form_name])
-
-        # Revisions should always correspond to transactions!
-        @transaction.commit_on_success
-        @revision.create_on_success
-        def _try_saving():
-            _check('gear_owner')
-            gear_owner = forms['gear_owner'].save()
-            entanglement.gear_owner_info = gear_owner
-            entanglement.save()
-            
-            if forms['gear_owner'].cleaned_data['location_set_known']:
-                _check('location_set')
-                gear_owner.location_gear_set = forms['location_set'].save()
-
-            gear_owner.save()
-
-            return entanglement
-
-        try:
-            return redirect(_try_saving())
-        except _SomeValidationFailed as (formname, form):
-            print "error in form %s: %s" % (formname, unicode(form.errors))
-
-    template_media = Media(
-        js= (settings.JQUERY_FILE, 'radiohider.js', 'checkboxhider.js'),
-    )
-    
-    return render_to_response(
-        'entanglements/add_gear_owner.html',
-        {
-            'case': entanglement,
-            'forms': forms,
-            'media': reduce( lambda m, f: m + f.media, forms.values(), template_media),
-        },
-        context_instance= RequestContext(request),
-    )
-
-@login_required
-@permission_required('entanglements.change_entanglement')
-@permission_required('entanglements.view_gear_owner')
 @permission_required('entanglements.change_gear_owner')
 def edit_gear_owner(request, entanglement_id):
     entanglement = Entanglement.objects.get(id=entanglement_id)
@@ -213,19 +149,16 @@ def edit_gear_owner(request, entanglement_id):
         'gear_owner': GearOwnerForm,
         'location_set': NiceLocationForm,
     }
+    model_instances = {}
+    form_initials = {}
 
-    form_initials = {
-        'gear_owner': {
-        }
-    }
-
-    if gear_owner.location_gear_set:
-        form_initials['gear_owner']['location_set_known'] = True
-
-    model_instances = {
-        'gear_owner': gear_owner,
-        'location_set': gear_owner.location_gear_set,
-    }
+    if not gear_owner is None:
+        model_instances['gear_owner'] = gear_owner
+        if gear_owner.location_gear_set:
+            model_instances['location_set'] = gear_owner.location_gear_set
+            if not 'gear_owner' in form_initials.keys():
+                form_initials['gear_owner'] = {}
+            form_initials['gear_owner']['location_set_known'] = True
 
     forms = {}
     for form_name, form_class in form_classes.items():
