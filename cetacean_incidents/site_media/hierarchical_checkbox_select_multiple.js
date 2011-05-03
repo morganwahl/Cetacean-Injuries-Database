fe882bee7831db50d0c2f704de8812532afa0167
Morgan Wahl <morgan.wahl@noaa.gov>
2011-05-03 13:55:13 -0400

    fixed attribute-updating, added superchecked attribute

--- cetacean_incidents/site_media/hierarchical_checkbox_select_multiple.js
+++ cetacean_incidents/site_media/hierarchical_checkbox_select_multiple.js
@@ -12,13 +12,24 @@ $().ready(function(){
         }
         
         if (this.checked) {
-            // add 'subchecked' class to all parent items
-            ul.find('li').has(this).addClass('subchecked');
+            li.find('li').addClass('superchecked');
+        } else {
+            var still_superchecked = ul.find('li.checked').find('li');
+            li.find('li').not(still_superchecked).removeClass('superchecked');
+        }
+        
+        if (this.checked) {
+            // add 'subchecked' class to all parent items that aren't children
+            // of a checked item
+            var checked_or_superchecked = ul.find('li.checked').add('li.superchecked');
+            ul.find('li').has(this).not(checked_or_superchecked).addClass('subchecked');
         } else {
             // removed 'subchecked' class from parents that don't still contain
             // a checked input
-            var still_checked = ul.find('li').has('input:checkbox:checked');
-            ul.find('li').has(this).not(still_checked).removeClass('subchecked');
+            var still_subchecked = ul.find('li').has('input:checkbox:checked');
+            ul.find('li').has(this).not(still_subchecked).removeClass('subchecked');
+        }
+    });
         }
     });
 });
