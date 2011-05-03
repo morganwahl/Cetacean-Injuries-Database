bb694a06021089b855770af7c6f1d2bbb1e1b044
Morgan Wahl <morgan.wahl@noaa.gov>
2011-05-03 11:09:49 -0400

    added TaxonMultipleChoiceField

--- cetacean_incidents/apps/taxons/forms.py
+++ cetacean_incidents/apps/taxons/forms.py
@@ -2,7 +2,14 @@ from django.core.validators import EMPTY_VALUES
 from django.core.urlresolvers import reverse
 from django.conf import settings
 from django import forms
+from django.forms.models import (
+    ModelChoiceIterator,
+    ModelMultipleChoiceField,
+)    
 from django.template.loader import render_to_string
+from django.utils.safestring import mark_safe
+
+from cetacean_incidents.apps.dag.widgets import HierarchicalCheckboxSelectMultiple
 
 from cetacean_incidents.apps.jquery_ui.widgets import ModelAutocomplete
 
@@ -46,6 +53,77 @@ class TaxonAutocomplete(ModelAutocomplete):
         css = {'all': (settings.JQUERYUI_CSS_FILE, 'taxon_autocomplete.css')}
         js = (settings.JQUERY_FILE, settings.JQUERYUI_JS_FILE, 'taxon_autocomplete.js')
 
+class TaxonChoiceIterator(ModelChoiceIterator):
+    # based on dag.forms.DAGModelChoiceIterator
+    
+    @staticmethod
+    def get_roots(queryset):
+        '''\
+        Filters out taxa that are _direct_ descendants of other taxa in this queryset.
+        '''
+        
+        return queryset.exclude(supertaxon__in=queryset.all())
+    
+    def _qs_to_choices(self, qs):
+        choices = []
+        pks = qs.values_list('pk', flat=True)
+        roots = self.get_roots(qs)
+        for root in roots:
+            # TODO this will actually include widget that shouldn't be in choices
+            children = root.subtaxa
+            if children.count():
+                choices.append((
+                    self.choice(root),
+                    self._qs_to_choices(children)
+                ))
+            else:
+                choices.append(self.choice(root))
+        
+        return tuple(choices)
+
+    def __iter__(self):
+        if self.field.empty_label is not None:
+            yield (u"", self.field.empty_label)
+        if self.field.cache_choices:
+            if self.field.choice_cache is None:
+                self.field.choice_cache = self._qs_to_choices(self.queryset)
+            for choice in self.field.choice_cache:
+                yield choice
+        else:
+            for choice in self._qs_to_choices(self.queryset):
+                yield choice
+
+class TaxonMultipleChoiceField(ModelMultipleChoiceField):
+    """
+    A MultipleChoiceField whose choices are Taxa.
+    """
+    # TODO merge with DAGField?
+    
+    widget = HierarchicalCheckboxSelectMultiple
+    
+    def label_from_instance(self, taxon):
+        label = u"<i>%s</i>" % taxon.scientific_name()
+        if taxon.common_names:
+            label += u"<br/>%s" % taxon.common_names
+        return mark_safe(label)
+    
+    def _get_choices(self):
+        # If self._choices is set, then somebody must have manually set
+        # the property self.choices. In this case, just return self._choices.
+        if hasattr(self, '_choices'):
+            return self._choices
+
+        # Otherwise, execute the QuerySet in self.queryset to determine the
+        # choices dynamically. Return a fresh QuerySetIterator that has not been
+        # consumed. Note that we're instantiating a new QuerySetIterator *each*
+        # time _get_choices() is called (and, thus, each time self.choices is
+        # accessed) so that we can ensure the QuerySet has not been consumed. This
+        # construct might look complicated but it allows for lazy evaluation of
+        # the queryset.
+        return TaxonChoiceIterator(self)
+
+    choices = property(_get_choices, ModelMultipleChoiceField._set_choices)
+    
 class TaxonField(forms.Field):
     # based on ModelChoiceField, except you can't choose queryset, and there
     # are no choices, since we're using an AJAX-y TaxonAutocomplete
