bde77bb168d7e62c671ad587b8419b9617a1d17f
Morgan Wahl <morgan.wahl@noaa.gov>
2011-05-03 13:55:59 -0400

    added expand/contract buttons

--- cetacean_incidents/site_media/hierarchical_checkbox_select_multiple.js
+++ cetacean_incidents/site_media/hierarchical_checkbox_select_multiple.js
@@ -1,6 +1,7 @@
 $().ready(function(){
     // update 'checked' class
-    var ul = $('ul.hierarchical_checkbox_select_multiple');
+    var UL_CLASS = 'hierarchical_checkbox_select_multiple';
+    var ul = $('ul.' + UL_CLASS);
     ul.children('li').find('input:checkbox').change(function(event){
         // 'this' is the checkbox
         // 'li' is the LI that contains the checkbox
@@ -30,7 +31,41 @@ $().ready(function(){
             ul.find('li').has(this).not(still_subchecked).removeClass('subchecked');
         }
     });
+    
+    // add the expand/contract links
+    // TODO remove hard-coded URL
+    var minus_url = '/site_media/icons/bullet_toggle_minus.png';
+    var minus_img = '<img class="button" title="contract" alt="&#x2212;" src="' + minus_url + '">';
+    var plus_url = '/site_media/icons/bullet_toggle_plus.png';
+    var plus_img = '<img class="button" title="expand" alt="&#x002b;" src="' + plus_url + '">';
+    // only prepend to li that contain sublists
+    ul.children('li').has('ul.' + UL_CLASS).prepend('<a href="" class="heir_toggle"></a>');
+    ul.find('a.heir_toggle').each(function(){
+        function expand(toggle) {
+            $(toggle).html(minus_img);
+            $(toggle).addClass('expanded');
+            $(toggle).nextAll('ul.' + UL_CLASS).show('fast');
         }
+        function contract(toggle) {
+            $(toggle).removeClass('expanded');
+            $(toggle).html(plus_img);
+            $(toggle).nextAll('ul.' + UL_CLASS).hide('fast');
+        }
+        // initialize the non-subchecked LIs to contracted
+        if ($(this).closest('li').hasClass('subchecked')) {
+            expand(this);
+        } else {
+            contract(this);
+        }
+        $(this).click(function(event){
+            // 'this' is now the 'a' that was clicked
+            if ($(this).hasClass('expanded')) {
+                contract(this);
+            } else {
+                expand(this);
+            }
+            return false;
+        });
     });
 });
 
