052b009ef4907131360d7b7886babeff95b33b76
Morgan Wahl <morgan.wahl@noaa.gov>
2011-06-01 11:15:28 -0400

    Added 'html' and 'link' filters to replace the <model>_link tags for each model

---
+++ cetacean_incidents/apps/generic_templates/templatetags/html_filter.py
@@ -0,0 +1,101 @@
+from django.core.cache import cache
+from django.db.models import Model
+from django import template
+from django.utils.safestring import mark_safe
+from django.template.loader import select_template, get_template
+
+register = template.Library()
+
+CACHE_TIMEOUT = 7 * 24 * 3600
+
+def cache_keys(obj):
+    # TODO repeats the key definition in html()
+    cache_key = u'%s__%s__%d__html' % (app_name, model_name, obj.id)
+    return (cache_key, cache_key + '__link')
+
+@register.filter
+def html(obj, link=False, use_cache=None):
+    '''\
+    Given a model instance, return HTML an HTML representation of that model.
+    '''
+    
+    if not isinstance(obj, Model):
+        return unicode(obj)
+    
+    options = {}
+    if hasattr(obj, 'get_html_options'):
+        options = obj.get_html_options()
+    
+    if use_cache is None and 'use_cache' in options:
+        use_cache = options['use_cache']
+    
+    # TODO caching requires and 'id' field
+    if not hasattr(obj, 'id'):
+        use_cache = False
+    
+    app_name = obj._meta.app_label
+    model_name = obj._meta.object_name.lower()
+    
+    if use_cache:
+        cache_key = u'%s__%s__%d__html' % (app_name, model_name, obj.id)
+        if link:
+            cache_key += '__link'
+        cached = cache.get(cache_key)
+        if cached:
+            print "cache hit: %s" % cache_key
+            return mark_safe(cached)
+    
+    if 'template' in options:
+        t = get_template(options['template'])
+    else:
+        # TODO we want to fall back to the default template included with this app;
+        # how to make that explicit?
+        template_names = [
+            "%s/%s.html" % (app_name, model_name),
+            'object.html',
+        ]
+        t = select_template(template_names)
+    
+    context = template.Context({
+        'object': obj,
+    })
+    
+    if link:
+        if hasattr(obj, 'get_absolute_url'):
+            context['url'] = obj.get_absolute_url()
+
+    if 'context' in options:
+        context.update(options['context'])
+    
+    html = t.render(context)
+    # strip whitespace from the ends
+    html = html.strip()
+    
+    if use_cache:
+        print "cache set: %s" % cache_key
+        cache.set(cache_key, html, CACHE_TIMEOUT)
+    
+    return mark_safe(html)
+
+@register.filter
+def html_cached(obj):
+    return html(obj, use_cache=True)
+
+@register.filter
+def htmls(objs, use_cache=None):
+    '''\
+    Applies the 'html' filter to an iterable of model instance and returns an
+    iterable of their HTML representation.
+    '''
+    
+    try:
+        return map(lambda o: html(o, use_cache), objs)
+    except TypeError:
+        # if not iterable, just return a list with one string for whatever was
+        # passed in
+        return [html(objs, use_cache)]
+
+@register.filter
+def htmls_cached(objs):
+    return htmls(objs, use_cache=True)
+
---
+++ cetacean_incidents/apps/generic_templates/templatetags/link_filter.py
@@ -0,0 +1,37 @@
+from django import template
+
+from html_filter import html, CACHE_TIMEOUT
+
+register = template.Library()
+
+@register.filter
+def link(obj, use_cache=None):
+    '''\
+    Given a model instance, return HTML for a link to that model, if it has a
+    'get_absolute_url()' method.
+    '''
+    
+    return html(obj, link=True, use_cache=use_cache)
+
+@register.filter
+def link_cached(obj):
+    return link(obj, use_cache=True)
+
+@register.filter
+def links(objs, use_cache=None):
+    '''\
+    Applies the 'link' filter to an iterable of model instance and returns an
+    iterable of their link HTML.
+    '''
+    
+    try:
+        return map(lambda o: link(o, use_cache), objs)
+    except TypeError:
+        # if not iterable, just return a list with one link to whatever was
+        # passed in
+        return [link(objs, use_cache)]
+
+@register.filter
+def links_cached(objs):
+    return links(objs, use_cache=True)
+
