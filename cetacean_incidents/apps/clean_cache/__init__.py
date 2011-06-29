a9cdc0a96c81bf7ca028131cdf80da47c7416830
Morgan Wahl <morgan.wahl@noaa.gov>
2011-06-29 12:05:45 -0400

    Complete re-write of clean_cache app. Finally seems to be a good design.

--- cetacean_incidents/apps/clean_cache/__init__.py
+++ cetacean_incidents/apps/clean_cache/__init__.py
@@ -1,3 +1,5 @@
+from operator import __not__, __or__
+
 from django.core.cache import cache
 
 from django.db import models
@@ -8,164 +10,157 @@ from django.contrib.contenttypes.models import ContentType
 
 CACHE_TIMEOUT = 7 * 24 * 60 * 60 # one week
 
-def register_cache_clear(
-        signal_name,
-        model,
-        key_pattern,
-        kwargs_filter=None,
-        name_suffix=None,
-        kwargs_map=None,
-    ):
-    if kwargs_filter is not None and name_suffix is None:
-        raise ValueError("Need a name_suffix if a kwargs_filter was passed in.")
-    if kwargs_filter is None:
-        kwargs_filter = lambda kwargs: True
-    if name_suffix is None:
-        name_suffix = signal_name
+class TestList(object):
     
-    if kwargs_map is None:
-        kwargs_map = lambda kwargs: {'pk': kwargs['instance'].pk}
+    def __init__(self, tests):
+        '''\
+        'tests' is an iterable of either callables or expressions. If any of
+        the expressions is True, this TestList will always be True. If any of
+        the expressions are False, they are ignored. If the list has callables
+        and no True expressions, the callables will be evaluated in order and
+        this TestList will be True if any of them are. If any empty list is
+        passed, this TestList will always be False.
+        '''
+        
+        self.tests = (False,)
+        
+        for t in tests:
+            if not callable(t) and bool(t):
+                self.tests = (True,)
+                return
+            if callable(t):
+                if self.tests == (False,):
+                    self.tests = []
+                self.tests.append(t)
+        
+        # if we built a list of callables, go ahead and make it a tuple
+        if isinstance(self.tests, list):
+            self.tests = tuple(self.tests)
+        
+    def test(self, arg):
+        if self.tests == (False,):
+            return False
+        if self.tests == (True,):
+            return True
+        for t in self.tests:
+            if t(arg):
+                return True
+        return False
     
-    name = '__'.join((
-        model._meta.app_label,
-        model._meta.object_name.lower(),
-        name_suffix,
-        key_pattern % {'pk': 'pk'},
-    ))
+    def __copy__(self):
+        # TestLists are immutable, so just return self
+        return self
     
-    def signal_handler(sender, **kwargs):
-        # sender should be model
-        if not kwargs_filter(kwargs):
-            return
-        cache_key = key_pattern % kwargs_map(kwargs)
-        cache.delete(cache_key)
-    # set a nice name for introspection's sake
-    signal_handler.__name__ = '%s_handler' % name
+    def __deepcopy__(self, memo):
+        # TestLists are immutable, so just return self
+        return self
     
-    signal = getattr(model_signals, signal_name)
+    def __repr__(self):
+        if self.tests == (False,) or self.tests == (True,):
+            return "TestList(%r)" % (self.tests,)
+        return "<TestList: %d callables>" % len(self.tests)
     
-    signal.connect(
-        receiver= signal_handler,
-        sender= model,
-        # very important to keep the signal handler from being garbage collected
-        weak= False,
-        # This is very important since it keeps duplicate handlers being created
-        # every time register_cache_clear is called with the same args
-        dispatch_uid= 'clean_cache__cache_clear__%s' % name
-    )
+    def __eq__(self, other):
+        return self.tests == other.tests
+    
+    def __ne__(self, other):
+        return self.tests != other.tests
+    
+    def __hash__(self):
+        return hash(self.tests)
+    
+    def __add__(self, other):
+        return TestList(self.tests + other.tests)
+    
+    def __unicode__(self):
+        if self.tests == (False,):
+            return "false TestList"
+        if self.tests == (True,):
+            return "true TestList"
+        return "%d-way TestList" % len(self.tests)
 
-def register_cache_clear_post_update(model, key_pattern):
-    
-    return register_cache_clear(
-        'post_save',
-        model,
-        key_pattern,
-        lambda kwargs: not kwargs['created'],
-        'post_update',
-    )
-    
-def register_cache_clear_post_create(model, key_pattern):
-    
-    return register_cache_clear(
-        'post_save',
-        model,
-        key_pattern,
-        lambda kwargs: kwargs['created'],
-        'post_create',
-    )
-
-def register_cache_clear_post_delete(model, key_pattern):
-    
-    return register_cache_clear(
-        'post_delete',
-        model,
-        key_pattern,
-    )
-
-def register_fk_cache_clears(model, fn, key_pattern):
-    # add signal handlers to detect object
-    # creation/deletion on the other end of a relation
-    
-    # TODO better way of doing this
-    for ro in model._meta.get_all_related_objects():
-        if ro.get_accessor_name() == fn:
-            break
-    
-    reverse_fn = ro.field.name
-    
-    def _kwargs_filter(kwargs):
-        inst = kwargs['instance']
-        reverse_value = getattr(inst, reverse_fn)
-        return not reverse_value is None
-    def _kwargs_map(kwargs):
-        inst = kwargs['instance']
-        reverse_value = getattr(inst, reverse_fn)
-        return {'pk': reverse_value.pk}
-    
-    def _pre_save_filter(kwargs):
-        # don't bother with new objects
-        return not kwargs['instance'].pk is None
-    
-    def _pre_save_map(kwargs):
-        inst = kwargs['instance']
-        # we actually need a new instance of the same entry to get the value
-        # currently in the database
-        old_inst = inst.__class__.objects.get(pk=inst.pk)
-        reverse_value = getattr(old_inst, reverse_fn)
-        return {'pk': reverse_value.pk}
-    
-    # both pre_save and post_save to catch both instances on the other side of
-    # relation
-    register_cache_clear(
-        signal_name= 'pre_save',
-        model= ro.model,
-        key_pattern= key_pattern,
-        kwargs_filter= _pre_save_filter,
-        name_suffix= 'pre_save_check_refs',
-        kwargs_map= _pre_save_map,
-    )
-    
-    register_cache_clear(
-        signal_name= 'post_save',
-        model= ro.model,
-        key_pattern= key_pattern,
-        kwargs_filter= _kwargs_filter,
-        name_suffix= 'post_save_check_refs',
-        kwargs_map= _kwargs_map,
-    )
-    
-    register_cache_clear(
-        signal_name= 'post_delete',
-        model= ro.model,
-        key_pattern= key_pattern,
-        kwargs_filter= _kwargs_filter,
-        name_suffix= 'post_delete_check_refs',
-        kwargs_map= _kwargs_map,
-    )
-
-class Smidgen(object):
+class CacheDependency(object):
     '''\
-    Class for descriptions of arbitrary bits of state of various Model
-    instances. Can produce State objects that represent the state of the bits
-    at the time they're created.
+    Class for descriptions of arbitrary bits of the database that might change,
+    invalidating a cache entry.
+    
+    'create' is a dictionary:
+    
+    {
+        <ContentType of model>: <TestList>,
+        ...
+    }
+    
+    If the Model instance of the class indicated by the key is updated and _any_
+    of the given tests pass, the cache key is invalid. A test can be a callable
+    that returns a boolean given the instance created, or a boolean expression.
+    
+    If the dictionary passed in uses the actual model classes as keys, they
+    will be converted to the above.
+
+    'update' is a dictionary:
+
+    {
+        (<ContentType of the model>, <primary key of Model instance>): <TestList>,
+        ...
+    }
+
+    If the dictionary passed in uses the actual model instances as keys, they
+    will be converted to the above.
+    
+    'delete' is a dictionary:
+    
+    {
+        (<ContentType of the model>, <primary key of Model instance>): <TestList>,
+        ...
+    }
+    
+    If the dictionary passed in uses the actual model instances as keys, they
+    will be converted to the above.
+    
+    This class is mainly useful for combining the cache-dependencies.
     '''
     
-    def __init__(self, description={}):
-        # fields is keyed to pairs consisting of a ContentType and a primary key
-        self.fields = {}
-        # these are used by any State instance created by this Smidgen
-        self.cache_key_patterns = {}
-        for obj, fieldnames in description.items():
-            # ignore None objects
+    # TODO could add model_update and model_delete args
+    
+    @staticmethod
+    def _simplify_testlist(tests):
+        for t in tests:
+            if not callable(t) and bool(t):
+                return (True,)
+        return tuple(filter(callable, tests))
+            
+    def __init__(self, create={}, update={}, delete={}):
+        # {
+        #   <ContentType of model>: <TestList>,
+        #   (<ContentType>, <pk>): <TestList>,
+        #   ...
+        # }
+        self.create = {}
+        self.update = {}
+        self.delete = {}
+        
+        for model, tests in create.items():
+            if not isinstance(model, ContentType):
+                if issubclass(model, Model):
+                    model = ContentType.objects.get_for_model(model)
+                else:
+                    raise ValueError(u'create keys must be a Model subclass or ContentType instance.')
+            
+            if model in self.create:
+                tests = self.create[model] + tests
+            self.create[model] = tests
+        
+        for obj, tests in update.items():
             if obj is None:
                 continue
-            
+
             # validate and convert the key
             if isinstance(obj, Model):
                 obj = (ContentType.objects.get_for_model(obj), obj.pk)
             
             if not len(obj) == 2:
-                raise ValueError("description keys must be either Model instances or pairs.")
+                raise ValueError(u'update keys must be either Model instances or pairs.')
             
             if not isinstance(obj, tuple):
                 obj = tuple(obj)
@@ -174,156 +169,63 @@ class Smidgen(object):
                 if issubclass(obj[0], Model):
                     obj = (ContentType.objects.get_for_model(obj[0]), obj[1])
                 else:
-                    raise ValueError(u'''description keys that are pairs must have a Model subclass or ContentType instance as their first item.''')
+                    raise ValueError(u'update keys that are pairs must have a Model subclass or ContentType instance as their first item.')
             
-            self.fields[obj] = set()
-            
-            # create cache key patterns to be used by State objects created by
-            # this Smidgen. Also, register signal handlers to clear the cache of
-            # those patterns
-            model = obj[0].model_class()
-            key_pattern = 'clean_cache__%s__%s__%%(pk)s' % (
-                model._meta.app_label,
-                model._meta.object_name.lower(),
-            )
-            # we may encound the same model more than once
-            if not model in self.cache_key_patterns:
-                missing_key_pattern = key_pattern + '__missing'
-                self.cache_key_patterns[model] = {
-                    'missing': missing_key_pattern,
-                    'fields': {},
-                }
-                register_cache_clear_post_create(model, missing_key_pattern)
-
-            # copy over fieldnames one at a time just to ensure what was passed in is indeed an iterable
-            for fieldname in fieldnames:
-                self.fields[obj].add(fieldname)
-
-                # create cache key patterns to be used by State objects created
-                # by this Smidgen. Also, register signal handlers to clear the
-                # cache of those patterns
-                if not fieldname in self.cache_key_patterns[model]['fields']:
-                    value_key_pattern = key_pattern + '__' + fieldname
-                    self.cache_key_patterns[model]['fields'][fieldname] = value_key_pattern
-                    register_cache_clear_post_update(model, value_key_pattern)
-                    register_cache_clear_post_delete(model, value_key_pattern)
+            if obj in self.update:
+                tests = self.update[obj] + tests
+            self.update[obj] = tests
         
+        for obj, tests in delete.items():
+            if obj is None:
+                continue
+
+            # validate and convert the key
+            if isinstance(obj, Model):
+                obj = (ContentType.objects.get_for_model(obj), obj.pk)
+            
+            if not len(obj) == 2:
+                raise ValueError(u'delete keys must be either Model instances or pairs.')
+            
+            if not isinstance(obj, tuple):
+                obj = tuple(obj)
+            
+            if not isinstance(obj[0], ContentType):
+                if issubclass(obj[0], Model):
+                    obj = (ContentType.objects.get_for_model(obj[0]), obj[1])
+                else:
+                    raise ValueError(u'delete keys that are pairs must have a Model subclass or ContentType instance as their first item.')
+            
+            if obj in self.delete:
+                tests = self.delete[obj] + tests
+            self.delete[obj] = tests
+
     def __or__(self, other):
-        if not isinstance(other, Smidgen):
+        if not isinstance(other, CacheDependency):
             return NotImplemented
         
-        new_desc = {}
+        kwargs = {
+            'create': {},
+            'update': {},
+            'delete': {},
+        }
         
-        objs = set(self.fields.keys()) | set(other.fields.keys())
-        for obj in objs:
-            new_desc[obj] = set()
-            if obj in self.fields:
-                new_desc[obj] |= self.fields[obj]
-            if obj in other.fields:
-                new_desc[obj] |= other.fields[obj]
-        
-        return Smidgen(new_desc)
+        for cd in (self, other):
+            for attr in ('create', 'update', 'delete'):
+                for key, tests in getattr(cd, attr).items():
+                    if key in kwargs[attr]:
+                        tests = kwargs[attr][key] + tests
+                    kwargs[attr][key] = tests
+
+        return CacheDependency(**kwargs)
+    
+    def __copy__(self):
+        # CacheDependencies are immutable, so just return self
+        return self
+    
+    def __deepcopy__(self, memo):
+        # CacheDependencies are immutable, so just return self
+        return self
     
     def __repr__(self):
-        return "Smidgen(%s)" % repr(self.fields)
-    
-    def current_state(self):
-        return State(self)
-
-class State(object):
-    '''\
-    An arbitrary bit of state of the database. The particular bit it is is can
-    be described by a Smidgen.
-    '''
-    
-    class NonExistantFieldValue(object):
-        # essentially just a wrapper around a string
-        
-        def __init__(self, fieldname):
-            self.fieldname = fieldname
-        
-        def __eq__(self, other):
-            if not isinstance(other, State.NonExistantFieldValue):
-                return False
-            
-            return self.fieldname == other.fieldname
-        
-        def __ne__(self, other):
-            return not self.__eq__(other)
-        
-        def __hash__(self):
-            return hash(self.fieldname)
-        
-        def __repr__(self):
-            return u"NonExistantFieldValue(%s)" % repr(self.fieldname)
-
-    def __init__(self, smidgen=None):
-        # essentially just a wrapper around a dictionary keyed to Model
-        # instances (really, ContentTypes and primary key pairs) whose values
-        # are dictionaries keyed to fieldnames whose values are the values of
-        # those fields.
-        
-        # Instances that don't actually exist are represented by a None value.
-        
-        # Fields that don't actually exist are represented by a
-        # NonExistantFieldValue.
-        
-        if smidgen is None:
-            smidgen = Smidgen()
-        
-        self._state = {}
-        for obj, fns in smidgen.fields.items():
-            ct, pk = obj
-            model = ct.model_class()
-            
-            missing_key_pattern = smidgen.cache_key_patterns[model]['missing']
-            missing_key = missing_key_pattern % {'pk': pk}
-            missing = cache.get(missing_key)
-            
-            inst = None
-            if not missing:
-                try:
-                    inst = model.objects.get(pk=pk)
-                except model.DoesNotExist:
-                    cache.set(missing_key, True, CACHE_TIMEOUT)
-            
-            if inst is None:
-                self._state[obj] = None
-                continue
-            
-            fields = {}
-            for fn in fns:
-                if not hasattr(inst, fn):
-                    fields[fn] = State.NonExistantFieldValue(fn)
-                    continue
-                
-                value_key_pattern = smidgen.cache_key_patterns[model]['fields'][fn]
-                value_key = value_key_pattern % {'pk': pk}
-                value = cache.get(value_key, State.NonExistantFieldValue(fn))
-                
-                if value == State.NonExistantFieldValue(fn):
-                    value = getattr(inst, fn)
-                    if isinstance(value, Manager):
-                        # HACK
-                        register_fk_cache_clears(model, fn, value_key_pattern)
-                        
-                        # use a set so State.__eq__ stays correct
-                        value = set(value.all())
-                    cache.set(value_key, value, CACHE_TIMEOUT)
-
-                fields[fn] = value
-
-            self._state[obj] = fields
-    
-    def __eq__(self, other):
-        if not isinstance(other, State):
-            return False
-        
-        return self._state == other._state
-    
-    def __ne__(self, other):
-        return not self.__eq__(other)
-    
-    def __repr__(self):
-        return "<State: %s>" % repr(self._state)
+        return "CacheDependency(%r, %r, %r)" % (self.create, self.update, self.delete)
 
--- cetacean_incidents/apps/clean_cache/clearing_cache.py
+++ cetacean_incidents/apps/clean_cache/clearing_cache.py
@@ -1,23 +1,189 @@
-from django.core.cache import cache
+from copy import copy
+import uuid
+import base64
 
-def set(key, value, timeout, deps):
-    '''\
-    'deps' is a Smidgen instance that describes the fields that the cached value
-    depends on.
-    '''
+from django.core.cache import cache as django_cache
+from django.db.models import signals as model_signals
+
+from django.contrib.contenttypes.models import ContentType
+
+from . import (
+    CacheDependency,
+    TestList,
+)
+
+class ClearingHandler(object):
     
-    new_value = (deps, deps.current_state(), value)
-    cache.set(key, new_value, timeout)
-
-def get(key, default=None):
-    value = cache.get(key, default)
-    if value == default:
-        return default
-
-    deps, orig_state, orig_value = value
-    if deps.current_state() != orig_state:
-        cache.delete(key)
-        return default
+    def __init__(self, clearer, model, signal_name, kwargs_tests=None):
+        # keys are primary key values, values are lists of cache keys to delete
+        # when a signal for that instance comes in
+        self.clearer = clearer
+        self.model = model
+        self.signal_name = signal_name
+        self.kwargs_tests = kwargs_tests
+        
+        # {
+        #   'any': {
+        #     <TestList>: set([<cache_key>, <cache_key>, ...]),
+        #     ...
+        #   },
+        #   'pks': {
+        #     <pk>: {
+        #       <TestList>: set([<cache_key>, <cache_key>, ...]),
+        #       ...
+        #     },
+        #   }
+        # }
+        self.cache_keys = {
+            'any': {},
+            'pks': {}
+        }
+        
+        signal = getattr(model_signals, signal_name)
+        signal.connect(
+            receiver= self,
+            sender= model,
+            weak= True,
+            dispatch_uid= 'clean_cache__cache_clearer__%s__%s__%s' % (
+                model._meta.app_label,
+                model._meta.object_name.lower(),
+                signal_name,
+            )
+        )
+        
+        from pprint import pprint
+        pprint(('handler.__init__', model, signal_name, kwargs_tests))
     
-    return orig_value
+    def add_cache_key(self, cache_key, instance_tests=TestList((True,)), pk=None):
+        if pk is None:
+            if not instance_tests in self.cache_keys['any']:
+                self.cache_keys[instance_tests] = set()
+            self.cache_keys[instance_tests].add(cache_key)
+        else:
+            if not pk in self.cache_keys['pks']:
+                self.cache_keys['pks'][pk] = {}
+            if not instance_tests in self.cache_keys['pks'][pk]:
+                self.cache_keys['pks'][pk][instance_tests] = set()
+            self.cache_keys['pks'][pk][instance_tests].add(cache_key)
+    
+    def remove_cache_key(self, cache_key):
+
+        raise NotImplementedError
+        for cache_keys in self.cache_keys.values():
+            if cache_key in cache_keys:
+                from pprint import pprint
+                pprint(('forgetting', cache_key))
+                cache_keys.remove(cache_key)
+    
+    def __call__(self, sender, **kwargs):
+        from pprint import pprint
+        pprint(('called', self, kwargs))
+        if not self.kwargs_tests.test(kwargs):
+            return
+        from pprint import pprint
+        pprint(('passed', self, kwargs))
+        
+        to_remove = set()
+        inst = kwargs['instance']
+        
+        for testlist, keys in self.cache_keys['any'].items():
+            if testlist.test(inst):
+                to_remove |= keys
+        
+        if inst.pk in self.cache_keys['pks']:
+            for testlist, keys in self.cache_keys['pks'][inst.pk].items():
+                if testlist.test(inst):
+                    to_remove |= keys
+        
+        for cache_key in to_remove:
+            from pprint import pprint
+            pprint(('deleting', cache_key))
+            django_cache.delete(cache_key)
+            #self.clearer.remove(cache_key)
+    
+    def __repr__(self):
+        return u"<ClearingHandler: %r->%r>" % (self.model, self.signal_name)
+    
+    def __unicode__(self):
+        return u"ClearingHandler for %s->%s" % (self.model, self.signal_name)
+
+class CacheClearer(object):
+    
+    def __init__(self):
+        self.handlers = {}
+    
+    def add(self, cache_key, deps):
+
+        # TODO m2m changes?
+        
+        for change_type in ('create', 'update', 'delete'):
+            for key, tests in getattr(deps, change_type).items():
+                if isinstance(key, ContentType):
+                    ct = key
+                    pk = None
+                else:
+                    ct, pk = key
+
+                model = ct.model_class()
+                
+                selector = (model, change_type)
+                if not selector in self.handlers:
+                    signal_name, kwargs_tests = {
+                        'create': ('post_save', TestList([
+                            lambda kwargs: 'created' in kwargs and kwargs['created'],
+                        ])),
+                        'update': ('post_save', TestList([
+                            lambda kwargs: 'created' in kwargs and not kwargs['created'],
+                        ])),
+                        'delete': ('pre_delete', TestList([True])),
+                    }[change_type]
+                    
+                    self.handlers[selector] = ClearingHandler(self, model, signal_name, kwargs_tests)
+                self.handlers[selector].add_cache_key(cache_key, instance_tests=tests, pk=pk)
+
+    def remove(self, key):
+        for h in self.handlers.values():
+            h.remove_cache_key(key)
+
+
+class Cache(object):
+    
+    def __init__(self):
+        self.uuid = uuid.uuid1()
+        self.prefix = base64.b64encode(self.uuid.bytes)
+        self.clearer = CacheClearer()
+    
+    def set(self, key, value, timeout, deps):
+        '''\
+        'deps' is a CacheDependency instance that describes the fields that the
+        cached value depends on.
+        '''
+        
+        from pprint import pprint
+        pprint(('set', key, deps.create, deps.update, deps.delete))
+
+        self.clearer.add(key, deps)
+        # since we keep info about the entires in memory, mark the value with
+        # our UUID, and add the deps info with the entry
+        stored_value = (self.uuid, value)
+        django_cache.set(key, stored_value, timeout)
+
+    def get(self, key, default=None):
+        from pprint import pprint
+        pprint(('get', key))
+
+        stored_value = django_cache.get(key, default)
+        if stored_value == default:
+            return default
+        
+        # unpack out metadata about the value
+        uuid, value = stored_value
+        if uuid != self.uuid:
+            # this value was stored by a different Cache instance, and thus we
+            # need to ignore it.
+            return default
+        
+        return value
+
+cache = Cache()
 
