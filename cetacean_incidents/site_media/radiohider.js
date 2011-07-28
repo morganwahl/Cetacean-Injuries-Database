cab3f296f1148f3b7ed228aeb78181d9940c3ef5
Morgan Wahl <morgan.wahl@noaa.gov>
2011-07-28 10:50:33 -0400

    added backwards-compatible selector support to radiohider.js

--- cetacean_incidents/site_media/radiohider.js
+++ cetacean_incidents/site_media/radiohider.js
@@ -1,5 +1,5 @@
 var RadioHider = {
-    'init': function(radio_name, val_to_id_map) {
+    'init_with_selectors': function(radio_name, val_to_selector_map) {
         var radio_sel = "[name=" + radio_name + "]";
         // This is the hack for IE
         // see http://stackoverflow.com/questions/208471
@@ -10,15 +10,22 @@ var RadioHider = {
           });
         }
         $(radio_sel).change(function(event){
-            for (var val in val_to_id_map) {
-                var id = val_to_id_map[val];
+            for (var val in val_to_selector_map) {
+                var selector = val_to_selector_map[val];
                 if ($(this).val() == val)
-                    $('#' + id).show();
+                    $(selector).show();
                 else
-                    $('#' + id).hide();
+                    $(selector).hide();
             }
         });
         $(radio_sel).filter(':checked').change();
     }
 }
-
+RadioHider.init = function(radio_name, val_to_id_map) {
+    var val_to_selector_map = {};
+    for (var val in val_to_id_map) {
+        var id = val_to_id_map[val];
+        val_to_selector_map[val] = '#' + id;
+    }
+    RadioHider.init_with_selectors(radio_name, val_to_selector_map);
+};
