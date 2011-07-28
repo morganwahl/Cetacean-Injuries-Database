fe81eb10e7a42718c246e0f13718a5e894a21bde
Morgan Wahl <morgan.wahl@noaa.gov>
2011-07-28 15:46:17 -0400

    added selector support to checkboxhider.js

--- cetacean_incidents/site_media/checkboxhider.js
+++ cetacean_incidents/site_media/checkboxhider.js
@@ -1,5 +1,5 @@
 var CheckboxHider = {
-    'init': function(checkbox_name, element_id) {
+    'init_with_selectors': function(checkbox_name, element_selector) {
         var checkbox_sel = "[name=" + checkbox_name + "]";
         // This is the hack for IE
         // see http://stackoverflow.com/questions/208471
@@ -11,12 +11,14 @@ var CheckboxHider = {
         }
         $(checkbox_sel).change(function(event){
             if ($(this).attr("checked")) {
-                $('#' + element_id).show();
+                $(element_selector).show();
             } else {
-                $('#' + element_id).hide();
+                $(element_selector).hide();
             }
         });
         $(checkbox_sel).change();
     }
 }
-
+CheckboxHider.init = function(checkbox_name, element_id) {
+    CheckboxHider.init_with_selectors(checkbox_name, '#' + element_id);
+};
