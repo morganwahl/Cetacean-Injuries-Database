1908f091c832db6b3c990d3beede9e8f6e7deffe
Morgan Wahl <morgan.wahl@noaa.gov>
2011-08-23 18:45:23 -0400

    removed debug code

--- cetacean_incidents/apps/jquery_ui/widgets/checkbox_select_multiple.py
+++ cetacean_incidents/apps/jquery_ui/widgets/checkbox_select_multiple.py
@@ -6,8 +6,6 @@ from django.forms.widgets import CheckboxSelectMultiple as DjangoCheckboxSelectM
 class CheckboxSelectMultiple(DjangoCheckboxSelectMultiple):
     def render(self, name, value, attrs=None, choices=()):
         result = super(CheckboxSelectMultiple, self).render(name, value, attrs, choices)
-        from pprint import pprint
-        pprint((name, value, attrs, choices))
         script = u"""
             <a href="" id="%(name)s-all">check all</a>
             <a href="" id="%(name)s-none">uncheck all</a>
